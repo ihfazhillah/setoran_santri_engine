e5e0b65cacf16e23236c44236a26e87dea1d392c
ihfazhillah <mihfazhillah@gmail.com>
2016-10-09 13:52:10 +0700

    menambahkan dokumentasi di get_sudah_free

--- query_setoran.py
+++ query_setoran.py
@@ -1,4 +1,4 @@
-from setoran_models import *
+from setoran_models import Santri, select, count, left_join
 
 
 def get_belum_setor():
@@ -23,7 +23,11 @@ def get_sudah_murojaah_harus_ulang():
                      if setoran.jenis == 'murojaah' and setoran.lulus is False)
 
 def get_sudah_free():
-    """sudah free adalah ketika jenis setoran == murojaah dan jenis murojaah == tambah dan lulus kedua duanya == True """
+    """sudah free adalah ketika jenis setoran == murojaah dan jenis murojaah == tambah dan lulus kedua duanya == True
+
+    Trick:
+        - query pertama mendapatkan santri yang jenis murojaah dan jenis adalah tambah, dan disetiap row yang didapatkan harus status dari kelulusan adalah True
+        - query kedua untuk menfilter santri yang sudah memiliki 2 row dari setorans yang didapatkan dari query pertama """
     santri = left_join(santri for santri in Santri \
                      for setoran in santri.setorans \
                      if (setoran.jenis == 'murojaah' or setoran.jenis == 'tambah') and setoran.lulus is True)
