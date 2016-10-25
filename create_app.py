0906fb5b5b999302241146546a48d637def1a1cf
ihfazhillah <mihfazhillah@gmail.com>
2016-10-25 21:31:52 +0700

    add login_manager from flask_login

--- create_app.py
+++ create_app.py
@@ -7,6 +7,15 @@ def create_app(config_filename):
     app = Flask(__name__)
     app.config.from_object(config_filename)
 
+    from flask_login import LoginManager
+    from webmodules.auth.helpers import User
+
+    lm = LoginManager(app)
+
+    @lm.user_loader
+    def load_user(username):
+        User.get(username)
+
     from webmodules.index.routes import mod as front_page_mod
     from webmodules.santri.routes import mod as santri_mod
     from webmodules.auth.routes import mod as auth_mod
