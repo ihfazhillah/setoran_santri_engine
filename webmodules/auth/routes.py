2033e1d320c712e35157f7ec8644b75e9c62f6ae
ihfazhillah <mihfazhillah@gmail.com>
2016-10-25 21:32:53 +0700

    testing pass, login with right username and password

--- webmodules/auth/routes.py
+++ webmodules/auth/routes.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, flash, render_template, request, url_for
+from flask_login import login_user 
 from .forms import LoginForm
+from .helpers import User
 
 mod = Blueprint("auth", __name__, template_folder="templates")
 
@@ -7,6 +9,19 @@ mod = Blueprint("auth", __name__, template_folder="templates")
 @mod.route("/login", methods=['POST', 'GET'])
 def login():
     login_form = LoginForm(csrf_enabled=False)
+
+    if request.method == 'POST':
+        if login_form.validate_on_submit():
+            username = login_form.username.data
+            password = login_form.password.data
+
+            user = User(username, password)
+
+            if user.is_authenticated:
+                login_user(user)
+                flash("Login Success")
+                return redirect(url_for("front_page.index"))
+
 
     return render_template("auth/login.html",
                            login_form=login_form)
