1c2c605d0a26af4be1e7976c0a74449adde2f214
ihfazhillah <mihfazhillah@gmail.com>
2016-10-25 20:51:04 +0700

    register auth blueprint

--- create_app.py
+++ create_app.py
@@ -9,7 +9,10 @@ def create_app(config_filename):
 
     from webmodules.index.routes import mod as front_page_mod
     from webmodules.santri.routes import mod as santri_mod
+    from webmodules.auth.routes import mod as auth_mod
+
     app.register_blueprint(front_page_mod)
     app.register_blueprint(santri_mod, url_prefix="/santri")
+    app.register_blueprint(auth_mod, url_prefix="/auth")
 
     return app
