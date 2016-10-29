93ff3b3510a1f7899a3be08409917c9fe66744f0
ihfazhillah <mihfazhillah@gmail.com>
2016-10-29 23:07:28 +0700

    delete_santri_without_login now passed

--- webmodules/santri/routes.py
+++ webmodules/santri/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 from setoran_models import *
+
 
 mod = Blueprint("santri", __name__,
                 template_folder="templates")
@@ -11,3 +13,9 @@ def display(id_):
     santri = get(s for s in Santri if s.id == id_)
     setoran = select(s for s in Setoran if s.santri == santri)
     return render_template("santri/display.html", setoran=setoran, santri=santri)
+
+@mod.route("/delete/<id_>")
+@login_required
+def delete(id_):
+    return "this delete page"
+    
