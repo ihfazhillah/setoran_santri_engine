ffe1a4dfe46478d6763ba6837ab0afe7389b8d8b
ihfazhillah <mihfazhillah@gmail.com>
2016-10-30 22:49:43 +0700

    add validation for start_end

---
+++ webmodules/setoran/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import SelectField, StringField, RadioField
+from wtforms.validators import DataRequired, ValidationError
+from setoran_models import check_startend_format
+
+def start_end_validator(form, field):
+    if not check_startend_format(field.data):
+        raise ValidationError("Wrong value inserted, must digit separated by '\'")
+
+class SetoranForm(FlaskForm):
+    start = StringField(validators=[DataRequired(), start_end_validator])
+    end = StringField(validators=[DataRequired(), start_end_validator])
