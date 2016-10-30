3728d8373c0f15e82225d514996a8eb74959c745
ihfazhillah <mihfazhillah@gmail.com>
2016-10-30 20:42:34 +0700

    disable csrf in testing configuration

--- webmodules/santri/routes.py
+++ webmodules/santri/routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, flash, url_for, redirect
+from flask import Blueprint, render_template, flash, url_for, redirect, request
 from flask_login import login_required
 from setoran_models import *
+from .forms import SantriForm
 
 
 mod = Blueprint("santri", __name__,
@@ -23,7 +24,15 @@ def delete(id_):
         flash("Santri with id %s was removed." %id_, 'info')
         return redirect(url_for("front_page.index"))
 
-@mod.route("/edit/<id_>", methods=["POST", "GET"])
+@mod.route("/edit/<id_>", methods=["POST"])
 @login_required
 def edit(id_):
-    return ""
+    santri_form = SantriForm()
+
+    if santri_form.validate_on_submit():
+        nama = santri_form.nama.data
+        with db_session:
+            santri = get(s for s in Santri if s.id == id_)
+            santri.nama = nama
+            flash("Santri with id %s was edited." %id_, 'info')
+            return redirect(url_for("front_page.index"))
