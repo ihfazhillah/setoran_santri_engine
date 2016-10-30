62f0a49873d22c1b9b2df1b9896efc6cdab39ed9
ihfazhillah <mihfazhillah@gmail.com>
2016-10-30 21:05:53 +0700

    add DataRequired() validators, cause user cant submit empty form

--- webmodules/santri/forms.py
+++ webmodules/santri/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
+from wtforms.validators import DataRequired
 
 
 class SantriForm(FlaskForm):
-    nama = StringField()
+    nama = StringField(label='Nama', validators=[DataRequired()])
