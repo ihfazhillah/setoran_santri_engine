6e4ee7209422a8e481525b4a854b9f13c91a392f
ihfazhillah <mihfazhillah@gmail.com>
2016-10-31 06:46:09 +0700

    change \ with /

--- webmodules/setoran/forms.py
+++ webmodules/setoran/forms.py
@@ -5,7 +5,7 @@ from setoran_models import check_startend_format
 
 def start_end_validator(form, field):
     if not check_startend_format(field.data):
-        raise ValidationError("Wrong value inserted, must digit separated by '\'")
+        raise ValidationError("Wrong value inserted, must digit separated by '/'")
 
 class SetoranForm(FlaskForm):
     start = StringField(validators=[DataRequired(), start_end_validator])
