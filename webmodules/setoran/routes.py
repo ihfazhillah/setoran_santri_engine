e4ecbc776bda5b978caaec6e38299c14120d19e8
ihfazhillah <mihfazhillah@gmail.com>
2016-10-31 07:12:19 +0700

    passing test delete setoran

--- webmodules/setoran/routes.py
+++ webmodules/setoran/routes.py
@@ -1,13 +1,21 @@
-from flask import Blueprint, redirect, url_for, render_template
+from flask import Blueprint, redirect, url_for, render_template, flash
 from flask_login import login_required
 from setoran_models import *
+from .forms import SetoranForm
 
 
 
 mod = Blueprint("setoran", __name__,
                 template_folder="templates")
 
-@mod.route("/delete/<int:id_>")
+@mod.route("/delete/<id_>")
 @login_required
 def delete(id_):
-    pass
+    with db_session:
+        setoran = get(s for s in Setoran if s.id == id_)
+        santri = setoran.santri
+        setoran.delete()
+        
+        flash("Setoran with id %s from santri with id %s was removed." %(id_, santri.id), "info")
+
+        return redirect(url_for("santri.display", id_=santri.id))
