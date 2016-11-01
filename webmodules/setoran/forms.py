7c15ff71d0113dcae530354cdfd487373293e9e3
ihfazhillah <mihfazhillah@gmail.com>
2016-11-01 21:45:12 +0700

    fix flake8 issues

--- webmodules/setoran/forms.py
+++ webmodules/setoran/forms.py
@@ -3,13 +3,19 @@ from wtforms import SelectField, StringField, RadioField
 from wtforms.validators import DataRequired, ValidationError
 from setoran_models import check_startend_format
 
+
 def start_end_validator(form, field):
     if not check_startend_format(field.data):
-        raise ValidationError("Wrong value inserted, must digit separated by '/'")
+        raise ValidationError("Wrong value inserted, "
+                              "must digit separated by '/'")
+
 
 class SetoranForm(FlaskForm):
     santri = SelectField(validators=[DataRequired()])
     start = StringField(validators=[DataRequired(), start_end_validator])
     end = StringField(validators=[DataRequired(), start_end_validator])
-    jenis = RadioField(validators=[DataRequired()], choices=[("murojaah", "Murojaah"), ("tambah", "Tambah")])
-    lulus = RadioField(validators=[DataRequired()], choices=[(True, "Ya"), (False, "Tidak")])
+    jenis = RadioField(validators=[DataRequired()],
+                       choices=[("murojaah", "Murojaah"),
+                                ("tambah", "Tambah")])
+    lulus = RadioField(validators=[DataRequired()],
+                       choices=[(True, "Ya"), (False, "Tidak")])
