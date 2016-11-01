bee2d9f4e8e953859516719e90392571ef693937
ihfazhillah <mihfazhillah@gmail.com>
2016-11-01 20:58:21 +0700

    fix flake8 errors and warnings

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,26 +1,21 @@
 import os
 from flask_testing import TestCase
-from flask import current_app
 from create_app import create_app
 from query_setoran import get_belum_setor
-from setoran_models import *
-
-
+from setoran_models import db, populate_db, Santri, Setoran
+from pony.orm import db_session, select, get, desc
 
 
 class MyTest(TestCase):
 
     def create_app(self):
         app = create_app("flask_config.Testing")
-        
-
         return app
 
     def setUp(self):
         # db.disconnect()
         db.create_tables()
         populate_db()
-
 
     def tearDown(self):
         db.drop_all_tables(with_all_data=True)
@@ -36,8 +31,6 @@ class MyTest(TestCase):
         self.assert_message_flashed("Logout success.")
         self.assert_redirects(resp, "/")
 
-
-
     @db_session
     def test_index_has_context(self):
         """testing index, has this context:
@@ -50,12 +43,13 @@ class MyTest(TestCase):
         belum_setor = self.get_context_variable("belum_setor")
         setoran = self.get_context_variable("setoran")
         belum_setor_from_db = get_belum_setor()
-        sudah_setor_from_db = select(s for s in Santri if s not in belum_setor_from_db)
-        setoran_from_db = select(s for s in Setoran).order_by(desc(Setoran.timestamp))[:5]
+        sudah_setor_from_db = select(s for s in Santri
+                                     if s not in belum_setor_from_db)
+        setoran_from_db = select(s for s in Setoran
+                                 ).order_by(desc(Setoran.timestamp))[:5]
         self.assertEqual(list(belum_setor), list(belum_setor_from_db))
         self.assertEqual(list(sudah_setor), list(sudah_setor_from_db))
         self.assertEqual(list(setoran), list(setoran_from_db))
-
 
     @db_session
     def test_display_santri_id_one(self):
@@ -97,10 +91,11 @@ class MyTest(TestCase):
         self.assertEqual(username.type, 'StringField')
         self.assertEqual(password.name, 'password')
         self.assertEqual(password.type, 'PasswordField')
-        self.assertTrue(all(login_form.data[x] is None for x in login_form.data))
+        self.assertTrue(all(login_form.data[x] is None
+                            for x in login_form.data))
 
     def test_auth_login_page_with_post(self):
-        resp = self.client.post("/auth/login", 
+        resp = self.client.post("/auth/login",
                                 data={'username': 'username',
                                       'password': 'password'})
         self.assert_redirects(resp, "/")
@@ -112,7 +107,7 @@ class MyTest(TestCase):
                                       'password': 'wrong_password'})
         self.assert_message_flashed("Wrong username or password.", "warning")
         self.assert_200(resp)
-        self.assert_template_used("auth/login.html")        
+        self.assert_template_used("auth/login.html")
 
     def test_aut_login_page_with_incorrect_password(self):
         resp = self.client.post("/auth/login",
@@ -147,14 +142,14 @@ class MyTest(TestCase):
             self.assertNotIn("raffi", [s.nama for s in santries])
 
     def test_edit_santri_without_login(self):
-        resp = self.client.post("/santri/edit/1", 
+        resp = self.client.post("/santri/edit/1",
                                 data={"nama": "ihfazh"})
         self.assert_redirects(resp, "/auth/login?next=%2Fsantri%2Fedit%2F1")
         self.assert_message_flashed("You're not logged in", "warning")
 
     def test_edit_santri_after_login(self):
         self.login()
-        resp = self.client.post("/santri/edit/1", 
+        resp = self.client.post("/santri/edit/1",
                                 data={"nama": "ihfazh"})
         self.assert_redirects(resp, "/")
         self.assert_message_flashed("Santri with id 1 was edited.", "info")
@@ -183,7 +178,6 @@ class MyTest(TestCase):
 
         with db_session:
             self.assertEqual(select(s for s in Santri).count(), 6)
-
 
     def test_add_santri_logged_user(self):
         self.login()
@@ -217,10 +211,9 @@ class MyTest(TestCase):
     def test_delete_setoran(self):
         self.login()
         resp = self.client.get("/setoran/delete/1")
-        self.assert_message_flashed("Setoran with id 1 from santri with id 6 was removed.", "info")
+        self.assert_message_flashed("Setoran with id 1 from santri "
+                                    "with id 6 was removed.", "info")
         self.assert_redirects(resp, "/santri/display/6")
 
         with db_session:
             self.assertEqual(select(s for s in Setoran).count(), 5)
-
-
--- webmodules/setoran/routes.py
+++ webmodules/setoran/routes.py
@@ -1,12 +1,11 @@
-from flask import Blueprint, redirect, url_for, render_template, flash
+from flask import Blueprint, redirect, url_for, flash
 from flask_login import login_required
-from setoran_models import *
-from .forms import SetoranForm
-
+from setoran_models import db_session, Setoran, get
 
 
 mod = Blueprint("setoran", __name__,
                 template_folder="templates")
+
 
 @mod.route("/delete/<id_>")
 @login_required
@@ -15,7 +14,8 @@ def delete(id_):
         setoran = get(s for s in Setoran if s.id == id_)
         santri = setoran.santri
         setoran.delete()
-        
-        flash("Setoran with id %s from santri with id %s was removed." %(id_, santri.id), "info")
+
+        flash("Setoran with id %s from santri with id %s was removed."
+              % (id_, santri.id), "info")
 
         return redirect(url_for("santri.display", id_=santri.id))
